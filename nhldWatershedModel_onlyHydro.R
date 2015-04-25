c1c7e46ec1ad66c136cd088bd904bb61b293c07b
joneslabND <sjones20@nd.edu>
2015-04-25 00:02:12 -0400

    add ice cover

--- nhldWatershedModel_onlyHydro.R
+++ nhldWatershedModel_onlyHydro.R
@@ -1,25 +1,14 @@
 ##### just hydrologic model for now...
 
+# clear workspace
 rm(list=ls())
 
 setwd("/Volumes/JonesExternal/External/activeStuff/NHLD_Cmodel/NHLD_lakeCmodel")
 
-########## load utility functions
+########## load utility functions and packages
 source("nhldWatershedModel_onlyHydroSupporting.R")
 require(deSolve)
 require(LakeMetabolizer)
-
-# load watershed table
-#d=read.table("vilasSheds_5-18-14.txt",sep="\t",header=TRUE)
-
-#W2L=d$shedArea_m2/d$lakeArea_m2
-#toFix=which(W2L>100 & d$lakeArea_m2<10000)
-#d$shedArea_m2[toFix]=d$lakeArea_m2[toFix]*1.5
-
-#W2L=d$shedArea_m2/d$lakeArea_m2
-
-#RTs=d$lakeVol_m3/(dailyP/1000*d$shedAreaNoLake_m2-d$shedAreaNoLake_m2*shedETs/1000+dailyP/1000*d$lakeArea_m2)
-
 
 ######## load forcing and flux data
 setwd("/Volumes/JonesExternal/External/activeStuff/NHLD_Cmodel/VICforcings&output/PROC")
@@ -41,43 +30,57 @@ flux=read.table(paste("FLUX_",UNDERCcells[1],sep=""),header=FALSE)
 # Lat Long Year Month Day Hour NetLW_Wm2 NetSW_Wm2 LW_Wm2 SW_Wm2 AirTemp_dC atmPress_kPa windspeed_mS@10m relHumid_fraction
 force=read.table(paste("FORCE_",UNDERCcells[1],sep=""),header=FALSE)
 
-curForce=force[force[,3]==2012,]
-curForce=curForce[curForce[,4]%in%(5:9),]
-curFlux=flux[flux[,3]==2012,]
-curFlux=curFlux[curFlux[,4]%in%(5:9),]
+# starting year/month/day, ending year/mnth/day, & set up force/flux
+startYear=1950
+startMonth=1
+startDay=1
 
-curForce=force[force[,3]==2013,]
-curForce=curForce[curForce[,4]%in%(5:9),]
-curFlux=flux[flux[,3]==2013,]
-curFlux=curFlux[curFlux[,4]%in%(5:9),]
+endYear=2013
+endMonth=12
+endDay=31
 
-curFluxDOY=as.numeric(strftime(strptime(paste(curFlux[,4],curFlux[,5],curFlux[,3],sep="-"),format="%m-%d-%Y"),format="%j"))
-curForceDOY=as.numeric(strftime(strptime(paste(curForce[,4],curForce[,5],curForce[,3],sep="-"),format="%m-%d-%Y"),format="%j"))
+curForce=force[which(((force[,3]==startYear) & (force[,4]==startMonth) & (force[,5]==startDay) & (force[,6]==0))):which(((force[,3]==endYear) & (force[,4]==endMonth) & (force[,5]==endDay) & (force[,6]==23))),]
 
-curFluxDOY=curFluxDOY-min(curFluxDOY)+1
-curForceDOY=curForceDOY-min(curForceDOY)+1
+curFlux=flux[which(((flux[,3]==startYear) & (flux[,4]==startMonth) & (flux[,5]==startDay))):which(((flux[,3]==endYear) & (flux[,4]==endMonth) & (flux[,5]==endDay))),]
+
+curFluxDOY=1:nrow(curFlux)
+curForceDOY=rep(curFluxDOY,each=24)
 
 setwd("/Volumes/JonesExternal/External/activeStuff/NHLD_Cmodel/NHLD_lakeCmodel")
-###### forcing functions --> when running longer can just use January 1, 1950 as an epoch
+
+###### forcing functions
 dailyPrecip=approxfun(curFluxDOY,curFlux[,7],method="constant")
 dailyEvap=approxfun(curFluxDOY,curFlux[,8],method="constant")
 dailyRunoff=approxfun(curFluxDOY,curFlux[,6],method="constant")
 
+####### ice cover: using regressions from Finland ()
+# -> alternative: try looking at NTL data...; fit model with degree days?
 
+###***** this stuff will have troubles if ice on happens after january 1 ******###
+aprilForce=force[force[,4]==4,]
+novForce=force[force[,4]==11,]
 
+aprilAT=tapply(aprilForce[,11],aprilForce[,3],FUN=mean)
+novAT=tapply(novForce[,11],novForce[3],FUN=mean)
 
-####### seems somewhat reasonable for summertime run
+iceOnDOY=(novAT+28.8)/0.19+182	#equation for November from inland North -> has latest ice on; pick this one because study area much higher N
 
-#### to run longer term need to consider:
-# - ice cover:  shut off evap?  base on cumulative air temp in some way... --> look at lit.!
-# - winter time precip:  does VIC discriminate between snow and not?
+iceOffDOY=(aprilAT-49.7)/-0.16+182-365	#Coastal/South model for breakup; again earliest date, but want this bias...
 
+iceON=numeric(nrow(curFlux))
+years=sort(unique(curFlux[,3]))
+for(i in 1:length(years)){
+	curYear=curFlux[curFlux[,3]==years[i],]
+	curDOY=as.numeric(strftime(strptime(paste(curYear[,3],curYear[,4],curYear[,5],sep="-"),format="%Y-%m-%d"),format="%j"))
+	iceON[curFlux[,3]==years[i]]=((curDOY<iceOffDOY[names(iceOffDOY)==years[i]]) | (curDOY>iceOnDOY[names(iceOnDOY)==years[i]]))*1
+}
 
+####### load lake/watershed information
 UNDERCsheds=read.table("../NHLDwatershedDelineations/UNDERCsheds_4-24-15.txt",header=TRUE,sep="\t",stringsAsFactors=FALSE)
 
 GFLOWoutput=read.table("../gflowOutput_3-24-15/GFLOWperElementDischarge_4-24-15.txt",header=TRUE,sep="\t",stringsAsFactors=FALSE)
 
-# iterate through lakes  --> perhaps in future run all lakes at once????
+###### iterate through lakes  
 #for i in 1:nrow(d){
 	i=17		#Long Lake from UNDERCsheds
 	i=4		#Morris from UNDERCsheds
@@ -123,21 +126,27 @@ GFLOWoutput=read.table("../gflowOutput_3-24-15/GFLOWperElementDischarge_4-24-15.
 	H=stageSim-stageOut
 	QoutSim=ifelse(H>0,(C*L*H^1.5)*(60*60*24),0)
 	
-	hydroSumm=cbind(times,dailyRunoff(times)*curShedArea/1000,dailyPrecip(times)*curLakeArea/1000,rep(gwIn,length(times)),QoutSim,dailyEvap(times)*curLakeArea/1000,rep(gwOut,length(times)))
+	hydroSumm=cbind(times,dailyRunoff(times)*curShedArea/1000,dailyPrecip(times)*curLakeArea/1000,rep(gwIn,length(times)),QoutSim,dailyEvap(times)*curLakeArea/1000*(1-iceON),rep(gwOut,length(times)))
 	colnames(hydroSumm)=c("time","Qin","precip","GWin","Qout","evap","GWout")
 
 	dev.new()
 	par(mfrow=c(3,3))
-	plot(hydroSumm[,1],hydroSumm[,2],xlab="time",ylab="Qin (m3 d-1)",type='l',lwd=2)
-	plot(hydroSumm[,1],hydroSumm[,3],xlab="time",ylab="Precip (m3 d-1)",type='l',lwd=2)
-	plot(hydroSumm[,1],hydroSumm[,4],xlab="time",ylab="GWin (m3 d-1)",type='l',lwd=2)
-	plot(hydroSumm[,1],hydroSumm[,5],xlab="time",ylab="Qout (m3 d-1)",type='l',lwd=2)
-	plot(hydroSumm[,1],hydroSumm[,6],xlab="time",ylab="Evap (m3 d-1)",type='l',lwd=2)
-	plot(hydroSumm[,1],hydroSumm[,7],xlab="time",ylab="GWout (m3 d-1)",type='l',lwd=2)
-	plot(out[,1],out[,2],xlab="time",ylab="Volume (m^3)",type='l',lwd=2)
-	plot(out[,1],stageSim,xlab="time",ylab="stage (m)",type='l',lwd=2)
+	plot(hydroSumm[,1],hydroSumm[,2],xlab="time",ylab="Qin (m3 d-1)",type='l',lwd=1)
+	plot(hydroSumm[,1],hydroSumm[,3],xlab="time",ylab="Precip (m3 d-1)",type='l',lwd=1)
+	plot(hydroSumm[,1],hydroSumm[,4],xlab="time",ylab="GWin (m3 d-1)",type='l',lwd=1)
+	plot(hydroSumm[,1],hydroSumm[,5],xlab="time",ylab="Qout (m3 d-1)",type='l',lwd=1)
+	plot(hydroSumm[,1],hydroSumm[,6],xlab="time",ylab="Evap (m3 d-1)",type='l',lwd=1)
+	plot(hydroSumm[,1],hydroSumm[,7],xlab="time",ylab="GWout (m3 d-1)",type='l',lwd=1)
+	plot(out[,1],out[,2],xlab="time",ylab="Volume (m^3)",type='l',lwd=1)
+	plot(out[,1],stageSim,xlab="time",ylab="stage (m)",type='l',lwd=1)
 	
 	meanBudget=colMeans(hydroSumm[,-1],na.rm=TRUE)
 	
 	barplot(meanBudget*c(1,1,1,-1,-1,-1),names.arg=names(meanBudget))
 	
+	
+	
+annualMin=tapply(stageSim,curFlux[,3],FUN=min,na.rm=TRUE)
+dev.new()
+plot(as.numeric(names(annualMin)),annualMin-mean(annualMin,na.rm=TRUE),type='o',xlab="year",ylab="min lake level anomaly")
+abline(h=0,lty=2)
--- nhldWatershedModel_onlyHydroSupporting.R
+++ nhldWatershedModel_onlyHydroSupporting.R
@@ -8,11 +8,19 @@ timeStep<-function(t,X,params){
 		#########################
 		V=X[1]
 		stage=V/curLakeArea	
+		
+		curDayString=paste(curFlux[i,3],curFlux[i,4],curFlux[i,5],sep="_")
 				
-		streamQ=dailyRunoff(t)*curShedArea/1000			#m3 day-1	****** problem because of interpolating?
+		# because we are accounting for direct precip isn't this the snow on ice? 
+		# I guess we could "store" that precip until spring...
+		streamQ=dailyRunoff(t)*(curShedArea-curLakeArea)/1000			#m3 day-1	****** problem because of interpolating?
 		directPrecip=dailyPrecip(t)*curLakeArea/1000		#m3 day-1	****** problem because of interpolating?
-			
-		curEvap=dailyEvap(t)*curLakeArea/1000			#m3 day-1	****** problem because of interpolating?
+		
+		if(iceON[floor(t)]==1){
+			curEvap=0	#m3
+		}else{
+			curEvap=dailyEvap(t)*curLakeArea/1000			#m3 day-1	
+		}
 		
 		# assuming ogee crest spillway
 		# Q=C*L*H^(3/2), where:
